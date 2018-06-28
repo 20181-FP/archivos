8224033c5fecd316011160b2813e7100b6e9b0ed
AngelDX <angel.condori@gmail.com>
2018-06-28 15:03:21 -0500

    first commit

--- Archi01.java
+++ Archi01.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Archi01{
 	public static void main(String[] args) {
-		while(true){
+		int n=0;
+		while(n>=0){
 			Scanner reader = new Scanner(System.in);
 			System.out.print("n:");
-			int n = Integer.parseInt( reader.nextLine() );
-			System.out.println("2/n="+2/n);	
+			n = Integer.parseInt( reader.nextLine() );
+			try{
+				int d=2/n;
+				System.out.println("2/n="+d);
+			}catch (Exception e) {
+				System.out.println("Mierror="+e);
+			}
 		}
-		
-		
 
 	}
 }
