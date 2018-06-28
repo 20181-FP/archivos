5d63d0569d1693722f71a323e8e2b656fd5f37c5
AngelDX <angel.condori@gmail.com>
2018-06-28 14:53:45 -0500

    first commit

---
+++ Archi01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Archi01{
+	public static void main(String[] args) {
+		while(true){
+			Scanner reader = new Scanner(System.in);
+			System.out.print("n:");
+			int n = Integer.parseInt( reader.nextLine() );
+			System.out.println("2/n="+2/n);	
+		}
+		
+		
+
+	}
+}
